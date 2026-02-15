ca7ff98d00518786032dcb6e8f098c9956a079d2
fguard89-a11y <fguard89@gmail.com>
2026-02-15 09:33:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
-
+import cors from 'cors';
+    
 dotenv.config();
-const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors()); 
@@ -57,5 +57,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
 
 
